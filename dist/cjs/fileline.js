7c15eb69a44ac7520774699fab43c56999026398
Doga Budak <dogabudak@gmail.com>
2023-07-28 09:00:14 +0200

    fix: fixed rollup

--- dist/cjs/fileline.js
+++
@@ -1,6 +0,0 @@
-'use strict';const getEol=require('./getEol.js');function stringToLines(data, param) {
-    const eol = getEol.default(data, param);
-    const lines = data.split(eol);
-    const partial = lines.pop() || "";
-    return { lines: lines, partial: partial };
-}exports.stringToLines=stringToLines;//# sourceMappingURL=fileline.js.map
--- dist/cjs/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';Object.defineProperty(exports,'__esModule',{value:true});const Converter=require('./Converter.js');const helper = function (param, options) {
-    return new Converter.Converter(param, options);
-};
-helper["csv"] = helper;
-helper["Converter"] = Converter.Converter;exports.default=helper;//# sourceMappingURL=index.js.map

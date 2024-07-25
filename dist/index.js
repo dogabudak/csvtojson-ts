ea21575a61a00c2419076716e330f6672b18d07e
Doga Budak <dogabudak@gmail.com>
2024-07-25 23:36:51 +0200

    feat: remove dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Converter = void 0;
-var Converter_1 = require("./Converter");
-Object.defineProperty(exports, "Converter", { enumerable: true, get: function () { return Converter_1.Converter; } });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEseUNBQXNDO0FBQTlCLHNHQUFBLFNBQVMsT0FBQSJ9
